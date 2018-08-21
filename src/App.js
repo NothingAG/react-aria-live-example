76d1e1832b37bd57bc0ecf53a1ffbc611d404b55
Rey <rey@nothing.ch>
2018-08-21 11:13:52 +0200

    feat: update page titles and set focus
    Update the h1 page title on routing and set focus on the h1 element.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
 import { LiveAnnouncer, LiveMessage } from 'react-aria-live';
@@ -12,26 +12,26 @@ import {
 
 const Home = () => (
   <div>
-    <h2>Home</h2>
+    <p>Home page</p>
   </div>
 )
 
 const About = () => (
   <div>
-    <h2>About</h2>
+    <p>About page</p>
   </div>
 )
 
 const Topic = ({ match }) => (
   <div>
-    <h3>{match.params.topicId}</h3>
+    <h2>{match.params.topicId}</h2>
+    <p>Topic page for {match.params.topicId}</p>
   </div>
 )
 
 const Topics = ({ match }) => {
   return (
     <div>
-      <h2>Topics</h2>
       <ul>
         <li>
           <Link to={`${match.url}/rendering`}>
@@ -58,6 +58,27 @@ const Topics = ({ match }) => {
   )
 }
 
+// Just for more consistent access to the routing information and easier editing,
+// we're going save the list of pages and their titles in an object.
+// This is not really necessary, but for our example, it saves a switch/case.
+const PAGES = {
+  home: {
+    title: "Home",
+    route: "/",
+    component: Home
+  },
+  about: {
+    title: "About",
+    route: "/about",
+    component: About
+  },
+  topics: {
+    title: "Topics",
+    route: "/topics",
+    component: Topics
+  }
+};
+
 /**
  * MainView
  * 
@@ -68,39 +89,85 @@ const Topics = ({ match }) => {
  * to the route change.
  */
 class MainView extends Component {
+
+  // Default react-router properties we get through withRouter
   static propTypes = {
     match: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
     history: PropTypes.object.isRequired
   }
+
   
+
   state = {
+    // Used to set a message and page changes
     a11yMessage: '',
+    // Current page title set in the h1
+    pageTitle: 'My Website'
   };
 
-  render() {
-    console.log(this.props.location);
+  constructor(props) {
+    super(props)
 
+    // Create and element reference to change focus to the h1 element on page change
+    this.pageChangeFocusElement = createRef();
+    this.pages = PAGES;
+  }
+
+  componentDidUpdate(prevProps) {
+    // When the path changed, we want to set re-set the focus of the page
+    // similar on how it's done with server-side routing.
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      // This is just a very raw snipped to change page titles. You probably know
+      // many better ways to manage page titles.
+
+      // Get information about the page
+      const selectedPageKey = Object.keys(this.pages).find(key => {
+        const page = this.pages[key];
+        return page.route === this.props.location.pathname
+      });
+
+      if (selectedPageKey) {
+        // Change page title and aria-live message
+        const selectedPage = this.pages[selectedPageKey];
+        this.setState({
+          pageTitle: selectedPage.title,
+          a11yMessage: `Page changed to ${selectedPage.title}`
+        })
+      }
+
+      // Change focus to the page title
+      this.pageChangeFocusElement.current.focus()
+    }
+  }
+
+  render() {
     return (
       <div>
-        <LiveMessage message={this.props.location.pathname} aria-live="polite" />
+        {/* LiveMessage will notify the user about the route change */}
+        <LiveMessage message={this.state.a11yMessage} aria-live="polite" />
+        <h1 ref={this.pageChangeFocusElement}>
+          {this.state.pageTitle}
+        </h1>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/topics">Topics</Link></li>
+          <li><Link to={this.pages.home.route}>{this.pages.home.title}</Link></li>
+          <li><Link to={this.pages.about.route}>{this.pages.about.title}</Link></li>
+          <li><Link to={this.pages.topics.route}>{this.pages.topics.title}</Link></li>
         </ul>
 
         <hr />
 
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        <Route exact path={this.pages.home.route} component={this.pages.home.component} />
+        <Route path={this.pages.about.route} component={this.pages.about.component} />
+        <Route path={this.pages.topics.route} component={this.pages.topics.component} />
       </div>
     )
   }
 };
 
+// Create an instance withRouter to be able to access all routing informations
+// inside the component.
 const MainViewWithRouter = withRouter(MainView);
 
 /**
@@ -112,6 +179,7 @@ export class App extends Component {
   render() {
     return (
       <LiveAnnouncer>
+        {/* LiveAnnouncer is the aria live wrapper needed */}
         <Router>
           <MainViewWithRouter />
         </Router>
